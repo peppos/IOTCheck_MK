3a423cc50eab046c2968780a11c126008a779472
Maxltrm <massimo.minniti90@gmail.com>
2019-06-12 15:57:30 +0200

    add venv and requirements

--- IOTCheck_mk.py
+++ IOTCheck_mk.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-import configparser, request
+import configparser, requests
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-print(config['cmk_auth']['CheckMKMaster'])
+cmk = dict(config._sections['cmk_auth'])
+
+uri = "http://{checkmkmaster}/{checkmksite}/check_mk/view.py?neg_opthost_group=&opthost_group={checkmkhostgroup}&st1=on&st2=on&st3=&stp=&view_name={checkmkview}&_username={checkmkautomationuser}&_secret={checkmkautomationpassword}&request_format=python&output_format=json".format(**cmk)
+
+output = requests.get(uri)
